79404fabb259d1ee996cf6e3b0eef94daf7fcf04
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-23 21:37:55 -0300

    23AGO24 - Aula 318 - Concatenação

---
+++ 317-concaten/script.php
@@ -0,0 +1,17 @@
+<?php
+
+  $nome = 'Yago';
+  $cor = 'azul';
+  $idade = 32;
+  $atividade = 'lutar boxe';
+
+  // Operador "."
+  echo 'Olá ' . $nome . ', vi que sua cor preferida é ' . $cor . ', vi também que você possui ' . $idade . ' anos e que gosta de ' . $atividade .'.';
+
+  // Aspas duplas
+  echo '<br>';
+
+  echo "Olá $nome, vi que sua cor preferida é $cor, vi também que você possui $idade anos e que gosta de $atividade";
+
+?>
+
