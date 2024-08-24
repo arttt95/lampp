595e5b89d890956b0b687b5f763ca8ab0756a8ae
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-24 12:48:41 -0300

    24AGO24 - Aula 318 - Variáveis contantes

---
+++ 318-var-const/script.php
@@ -0,0 +1,15 @@
+<?php
+
+define ('BD_URL', 'endereco_bd_dev');
+define('BD_USUARIO', 'usuario_dev');
+define('BD_SENHA', 'senha_dev');
+
+// .. Lógica .. //
+
+
+echo BD_URL . '<br>';
+echo BD_USUARIO . '<br>';
+echo BD_SENHA . '<br>';
+
+?>
+
