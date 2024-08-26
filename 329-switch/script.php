04133b3e8e5dfaa7df49b3f2f158ac396a96dafd
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-26 07:13:38 -0300

    26AGO24 - Aula 329 - Switch

---
+++ 329-switch/script.php
@@ -0,0 +1,25 @@
+<?php
+
+$parametro = 'abc';
+
+switch ($parametro) {
+
+    case 1:
+        echo 'Entrou no case 1';
+        break;
+    
+    case 2:
+        echo 'Entrou no case 2';
+        break;
+    
+    case 3:
+        echo 'Entrou no case 3';
+        break;
+
+    default:
+        echo 'Entrou no default';
+        break;
+
+}
+
+?>
