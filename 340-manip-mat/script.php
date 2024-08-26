9ab3ef9ab3a54d741ed8985ca711339ed4de4d49
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-26 20:45:05 -0300

    26AGO24 - Aula 340 - Funções nativas para tarefas matemáticas.

---
+++ 340-manip-mat/script.php
@@ -0,0 +1,25 @@
+<?php
+
+echo ceil(7.35); // arredonda para cima => 8
+
+echo '<br>';
+
+echo floor(7.35); // arredonda para baixo => 7
+
+echo '<br>';
+
+echo round(7.35); // arredonda com base na fração => 7
+
+echo '<br>';
+
+echo rand(0, 300); // gera um valor aleatório entre 0 e 300
+
+echo '<br>';
+
+echo getrandmax(); // retorna o valor máximo aleatório possível na máquina
+
+echo '<br>';
+
+echo sqrt(15); // retorna a raiz quadrada do número passado integer ou float
+
+?>
