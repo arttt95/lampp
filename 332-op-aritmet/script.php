03fcdea28c82895debdddbeeb09f793a683c15bd
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-26 13:47:18 -0300

    26AGO24 - Aula 332 - Praticando com operadores aritméticos.

---
+++ 332-op-aritmet/script.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = 13;
+$num2 = 4;
+
+echo "A soma entre $num1 e $num2 é " . ($num1 + $num2);
+echo "<br>";
+echo "A subtração entre $num1 e $num2 é " . ($num1 - $num2);
+echo "<br>";
+echo "A multiplicação entre $num1 e $num2 é " . ($num1 * $num2);
+echo "<br>";
+echo "A divisão entre $num1 e $num2 é " . ($num1 / $num2);
+echo "<br>";
+echo "O resto entre $num1 e $num2 é " . ($num1 % $num2);
+
+?>
