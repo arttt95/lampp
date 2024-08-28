bd3dbbb2c01428cc2c886b3218896c8161967936
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-28 10:14:20 -0300

    28AGO24 - Aula 351 - Loops (For).

---
+++ 351-for/script.php
@@ -0,0 +1,29 @@
+<?php
+
+while (false) {
+
+    // Lógica
+    break;
+
+}
+
+do {
+
+    // Lógica
+    break;
+
+} while (false);
+
+for ($x = 10; true; $x--) {
+
+    if ($x == 0) {
+
+        break;
+
+    }
+
+    echo "X = $x<br>";
+
+}
+
+?>
