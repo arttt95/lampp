d350724191e43aea95680660015d869deb056abe
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-31 16:54:31 -0300

    31AGO24 - Aula 362 - Help Desk (Incorporando scripts com include(), include_once(), require() e require_once()).

---
+++ 999-incorporando-scripts/inicio.php
@@ -0,0 +1,19 @@
+<?php
+
+    //include_once("menu.php"); // Gera um warning ao identificar erros
+
+    require_once("menu.php"); // Gera um fatal error ao identificar erros
+
+?>
+
+Conteúdo da página (início)
+
+<br><br>
+
+<?php
+
+    //include_once("menu.php"); // Gera um warning ao identificar erros
+
+    require_once("menu.php"); // Gera um fatal error ao identificar erros
+
+?>
---
+++ 999-incorporando-scripts/vagas.php
@@ -0,0 +1,7 @@
+<?php
+
+    include("menu.php");
+    
+?>
+
+Conteúdo da página (vagas)
