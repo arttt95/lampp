139c67e63313f4cfa3539843d71517d697fe576f
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-09-29 16:40:47 -0300

    29SET24 - Aula 454 - XML (Parte 3 - Convertendo o XML para JSON - #document; DOMParser; xmlToJson).

---
+++ 454-xml-pt3/script.js
@@ -0,0 +1,36 @@
+function getFilmes() {
+
+    let xmlHttp = new XMLHttpRequest()
+    xmlHttp.open('GET', 'http://localhost/454-xml-pt3/filmes.xml');
+
+    xmlHttp.onreadystatechange = () => {
+
+        if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+
+            let XMLFilmes = xmlHttp.responseText;
+            console.log(XMLFilmes);
+            
+            let parser = new DOMParser();
+
+            domFilmes = parser.parseFromString(XMLFilmes, 'text/xml');
+
+            console.log(domFilmes);
+
+            jsonFilmes = xmlToJson(domFilmes);
+
+            console.log(jsonFilmes);
+
+            console.log('Yeah buddy!')
+
+        }
+
+        if(xmlHttp.readyState == 4 && xmlHttp.status == 404) {
+
+            //...
+
+        }
+
+    }
+
+    xmlHttp.send()
+}
