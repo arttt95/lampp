03fe93cd291d5f7e1430b4033c4e566b8f1b1dc6
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-17 04:00:42 -0300

    17OUT24 - Aula 477 - App To Do (Listando todos os registros - Parte 2).

--- 477-to-do-07/todas_tarefas.php
+++ 477-to-do-07/todas_tarefas.php
@@ -4,6 +4,12 @@ $acao = 'recuperar';
 
 require 'tarefa_controller.php';
 
+/*
+echo '<pre>';
+print_r($tarefas);
+echo '</pre>';
+*/
+
 ?>
 
 <html>
@@ -44,8 +50,14 @@ require 'tarefa_controller.php';
 								<h4>Todas tarefas</h4>
 								<hr />
 
+								<?php
+
+								foreach($tarefas as $i => $tarefa) {
+
+								?>
+
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
+									<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
@@ -53,14 +65,7 @@ require 'tarefa_controller.php';
 									</div>
 								</div>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php } ?>
 								
 							</div>
 						</div>
